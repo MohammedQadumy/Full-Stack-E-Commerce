c70fee4a1bb6e436e73a16ac4f01288842861d05
mohammedQadumy <mohammadqadumy@gmail.com>
2024-05-13 21:55:10 +0300

    shopping cart added without details

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -3,11 +3,14 @@ package com.luv2code.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @SpringBootApplication
 public class SpringBootEcommerceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootEcommerceApplication.class, args);
 	}
-
 }
